9861d2bd455e6cb8d984df83c743f4e8d01767b4
이경진 <bramhome5848@gmail.com>
2020-05-15 22:21:49 +0900

    20200515 ~p118 && CRUD Code

--- src/main/java/com/lkj/book/service/posts/PostsService.java
+++ src/main/java/com/lkj/book/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.lkj.book.service.posts;
 
+import com.lkj.book.domain.posts.Posts;
 import com.lkj.book.domain.posts.PostsRepository;
+import com.lkj.book.web.dto.PostsResponseDto;
 import com.lkj.book.web.dto.PostsSaveRequestDto;
+import com.lkj.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +18,24 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        //엔티티의 값 변경 -> commit시에 변경감지
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
---
+++ src/main/java/com/lkj/book/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package com.lkj.book.web.dto;
+
+import com.lkj.book.domain.posts.Posts;
+import lombok.Data;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
