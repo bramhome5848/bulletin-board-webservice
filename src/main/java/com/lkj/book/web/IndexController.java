b8e3909eb691f457ed6f00c0172fb2341fc84fbf
이경진 <bramhome5848@gmail.com>
2020-06-14 19:00:10 +0900

    20200613 annotation & session object save in db

--- src/main/java/com/lkj/book/web/IndexController.java
+++ src/main/java/com/lkj/book/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.lkj.book.web;
 
+import com.lkj.book.config.auth.LoginUser;
 import com.lkj.book.config.auth.dto.SessionUser;
 import com.lkj.book.service.posts.PostsService;
 import com.lkj.book.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDsc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
