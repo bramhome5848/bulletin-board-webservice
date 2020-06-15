8e0b1e8ddf88899cf50868831991a00b0879ed35
이경진 <bramhome5848@gmail.com>
2020-06-15 22:30:27 +0900

    20200615 Test Code fix it

--- src/main/java/com/lkj/book/BulletinBoardWebserviceApplication.java
+++ src/main/java/com/lkj/book/BulletinBoardWebserviceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing	//JPA Auditing활성화
+//@EnableJpaAuditing	//JPA Auditing활성화 -> JpaConfig로 이동
 public class BulletinBoardWebserviceApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/lkj/book/web/HelloControllerTest.java
+++ src/test/java/com/lkj/book/web/HelloControllerTest.java
@@ -1,8 +1,12 @@
 package com.lkj.book.web;
 
+import com.lkj.book.config.auth.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,13 +23,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @WebMvcTest의 경우는 Controller layer를 테스트하고 모의 객체를 사용하기 때문에 나머지 필요한 bean을 직접 세팅.
  * 그렇기 때문에 가볍고 빠르게 테스트 가능.
  */
-@WebMvcTest(HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
+    @WithMockUser(roles="USER")
     public void hello가_리턴된다() throws Exception {
         //given
         String hello = "hello";
@@ -41,6 +50,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void helloDto가_리턴된다() throws Exception {
 
         //given
